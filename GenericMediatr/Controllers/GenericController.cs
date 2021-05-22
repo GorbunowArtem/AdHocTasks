41962b0c19540740f61ad718cc4613ee4088913d
Artem Horbunov <gorbunowartemwork@gmail.com>
2021-05-22 20:28:35 +0300

    Added docker-compose for local development

---
+++ GenericMediatr/Controllers/GenericController.cs
@@ -0,0 +1,20 @@
+﻿using CFX;
+using CFX.Production;
+using CFX.Production.TestAndInspection;
+using CFX.ResourcePerformance;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericMediatr.Controllers
+{
+	[ApiController]
+	public class GenericController: ControllerBase
+	{
+		private readonly CFXMessage[] _messages = {
+			new FaultCleared(),
+			new FaultOccurred(),
+			new UnitsInspected(),
+			new RecipeActivated()
+		};
+
+	}
+}
